531475b52c8ef26797e6842ed3cc366c9df2655d
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-10-09 21:41:15 -0300

    Adicionando JpaSpecification para customer

---
+++ VideoLocadora/src/main/java/io/github/gabryel/videolocadora/repository/customer/CustomerSpecs.java
@@ -0,0 +1,16 @@
+package io.github.gabryel.videolocadora.repository.customer;
+
+import io.github.gabryel.videolocadora.entity.customer.CustomerEntity;
+import org.springframework.data.jpa.domain.Specification;
+
+public abstract class CustomerSpecs {
+
+    public static Specification<CustomerEntity> nameLike(String name) {
+        return (root, query, cb) -> cb.like(cb.upper(root.get("name")), "%" + name + "%".toUpperCase());
+    }
+
+    public static Specification<CustomerEntity> emailLike(String email) {
+        return (root, query, cb) -> cb.like(cb.upper(root.get("email")), "%" + email + "%".toUpperCase());
+    }
+
+}
