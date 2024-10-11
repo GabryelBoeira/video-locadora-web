c08c330623c8aebedc90df4786d795b5583e3068
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-10-11 20:40:11 -0300

    Trabalhando na consulta na camada de servico

--- VideoLocadora/src/main/java/io/github/gabryel/videolocadora/service/customer/CustomerService.java
+++ VideoLocadora/src/main/java/io/github/gabryel/videolocadora/service/customer/CustomerService.java
@@ -1,6 +1,7 @@
 package io.github.gabryel.videolocadora.service.customer;
 
 import io.github.gabryel.videolocadora.entity.customer.CustomerEntity;
+import io.github.gabryel.videolocadora.exception.BusinessException;
 import io.github.gabryel.videolocadora.repository.customer.CustomerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,8 +18,15 @@ public class CustomerService {
         customerRepository.save(customer);
     }
 
-    public CustomerEntity findById(Long id) {
-        return customerRepository.findById(id).orElse(null);
+    /**
+     * Finds a customer by ID.
+     *
+     * @param id the ID of the customer to be found
+     * @return the customer found
+     * @throws BusinessException if the customer is not found
+     */
+    public CustomerEntity findById(Long id) throws BusinessException {
+        return customerRepository.findById(id).orElseThrow(() -> new BusinessException("Customer not found"));
     }
 
     public List<CustomerEntity> findAll() {
@@ -29,6 +37,12 @@ public class CustomerService {
         customerRepository.deleteById(id);
     }
 
+    /**
+     * Finds a customer by CPF.
+     *
+     * @param cpf the customer's CPF
+     * @return a list containing the customer found
+     */
     public List<CustomerEntity> findByCpf(String cpf) {
         return customerRepository.findByCpf(cpf);
     }
