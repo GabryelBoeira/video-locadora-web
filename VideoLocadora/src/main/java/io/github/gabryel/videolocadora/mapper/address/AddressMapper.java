a05381f818f69cf1a4f5c4f64db169cd553885d2
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2025-04-26 20:31:35 -0300

    Realizando implementações no mapper
    Vinculado o converter do endereço direto no mapper do cliente, para remover complexidade do codigo ao que o sistema ja faz automatico

--- VideoLocadora/src/main/java/io/github/gabryel/videolocadora/mapper/address/AddressMapper.java
+++ VideoLocadora/src/main/java/io/github/gabryel/videolocadora/mapper/address/AddressMapper.java
@@ -2,10 +2,12 @@ package io.github.gabryel.videolocadora.mapper.address;
 
 import io.github.gabryel.videolocadora.dto.address.AddressDetailDTO;
 import io.github.gabryel.videolocadora.dto.address.AddressSaveDTO;
-import io.github.gabryel.videolocadora.entity.customer.AddressEntity;
+import io.github.gabryel.videolocadora.entity.AddressEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.data.domain.Page;
+import org.mapstruct.Named;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface AddressMapper {
@@ -14,6 +16,24 @@ public interface AddressMapper {
     @Mapping(target = "active", ignore = true)
     @Mapping(target = "primaryAddress", ignore = true)
     @Mapping(target = "customer", ignore = true)
+    @Mapping(target = "zipCode", qualifiedByName = "removeFormatZipCode")
     AddressEntity toEntity(AddressSaveDTO addressSaveDTO);
+
+    List<AddressEntity> toEntityList(List<AddressSaveDTO> addressSaveDTOList);
+
+    @Mapping(target = "zipCode", qualifiedByName = "addFormatZipCode")
+    AddressDetailDTO toDetailDTO(AddressEntity addressEntity);
+
+    List<AddressDetailDTO> toDetailDTOList(List<AddressEntity> addressEntityList);
+
+    @Named("removeFormatZipCode")
+    default String removeFormatZipCode(String zipCode) {
+        return zipCode.replace(".", "").replace("-", "");
+    }
+
+    @Named("addFormatZipCode")
+    default String addFormatZipCode(String zipCode) {
+        return String.format("%s-%s", zipCode.substring(0, 5), zipCode.substring(5, 8));
+    }
 
 }
--- VideoLocadora/src/main/java/io/github/gabryel/videolocadora/mapper/customer/CustomerMapper.java
+++ VideoLocadora/src/main/java/io/github/gabryel/videolocadora/mapper/customer/CustomerMapper.java
@@ -1,30 +1,29 @@
 package io.github.gabryel.videolocadora.mapper.customer;
 
-import io.github.gabryel.videolocadora.dto.page.PagedResponseDTO;
 import io.github.gabryel.videolocadora.dto.customer.CustomerDetailDTO;
 import io.github.gabryel.videolocadora.dto.customer.CustomerSaveDTO;
-import io.github.gabryel.videolocadora.entity.customer.CustomerEntity;
+import io.github.gabryel.videolocadora.dto.page.PagedResponseDTO;
+import io.github.gabryel.videolocadora.entity.CustomerEntity;
+import io.github.gabryel.videolocadora.mapper.address.AddressMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.Named;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 
-@Mapper
+@Mapper(componentModel = "spring", uses = {AddressMapper.class})
 public interface CustomerMapper {
 
-    CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
-
+    @Mapping(source = "cpf", target = "cpf", qualifiedByName = "addFormatCpf")
     CustomerDetailDTO toDetailDTO(CustomerEntity customerEntity);
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "addresses", ignore = true)
     @Mapping(target = "delayDevolution", ignore = true)
+    @Mapping(source = "cpf", target = "cpf", qualifiedByName = "removeFormatCpf")
     CustomerEntity toEntity(CustomerSaveDTO customerSaveDTO);
 
     /**
@@ -35,7 +34,7 @@ public interface CustomerMapper {
      */
     default PagedResponseDTO<CustomerDetailDTO> toDetailPage(Page<CustomerEntity> customerPage) {
         if (customerPage == null || customerPage.isEmpty()) {
-            return new PagedResponseDTO<>(null, 0, 0, 0, 0, true, true);
+            return new PagedResponseDTO<>(new ArrayList<>(), 0, 0, 0, 0, true, true);
         }
 
         List<CustomerDetailDTO> customerDetailDTOList = customerPage.getContent().stream()
@@ -51,6 +50,16 @@ public interface CustomerMapper {
                 customerPage.isFirst(),
                 customerPage.isLast()
         );
+    }
+
+    @Named("removeFormatCpf")
+    default String removeFormatCpf(String cpf) {
+        return cpf.replace(".", "").replace("-", "");
+    }
+
+    @Named("addFormatCpf")
+    default String addFormatCpf(String cpf) {
+        return String.format("%s.%s.%s-%s", cpf.substring(0, 3), cpf.substring(3, 6), cpf.substring(6, 9), cpf.substring(9, 11));
     }
 
 }
